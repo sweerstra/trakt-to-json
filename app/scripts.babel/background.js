e6294f4ca6023727a9c3eb00d56e26fd9aa72322
fhict <fhict>
2017-01-05 17:14:22 +0100

    Deleted unused helpers

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,5 +1,3 @@
-'use strict';
-
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {
--- app/scripts.babel/chromereload.js
+++ app/scripts.babel/chromereload.js
@@ -1,12 +1,6 @@
-'use strict';
-
 const LIVERELOAD_HOST = 'localhost:';
 const LIVERELOAD_PORT = 35729;
 const connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
-
-connection.onerror = error => {
-    console.log('reload connection got error:', error);
-};
 
 connection.onmessage = e => {
     if (e.data) {
