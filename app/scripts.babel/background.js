e481297f106b08e425a02ef99ba0d179de5c7972
fhict <fhict>
2017-01-05 18:22:05 +0100

    Format background.js

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,16 +1,16 @@
 (function () {
-  chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
-        type: 'options',
-        options: fetchOptions(),
-        url: tabs[0].url
-      }, function (response) {
-      });
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                type: 'options',
+                options: fetchOptions(),
+                url: tabs[0].url
+            }, function (response) {
+            });
+        });
     });
-  });
 
-  function fetchOptions() {
-    return JSON.parse(localStorage.getItem('options'));
-  }
+    function fetchOptions() {
+        return JSON.parse(localStorage.getItem('options'));
+    }
 })();
