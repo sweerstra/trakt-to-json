47161171c9f2c04e8be8f4138d4ffb82775bb70b
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-02-03 00:49:43 +0100

    Use default options when no options are set

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -3,7 +3,12 @@
         chrome.tabs.query({active: true, currentWindow: true}, tabs => {
             chrome.tabs.sendMessage(tabs[0].id, {
                 type: 'options',
-                options: JSON.parse(localStorage.getItem('options')),
+                options: JSON.parse(localStorage.getItem('options')) || {
+                    type: 'all',
+                    sort: null,
+                    years: 0,
+                    amount: null
+                },
                 url: tabs[0].url
             }, null);
         });
