a714b5e2ec5c15db46a31e28d9d1177dd2cce709
RussianDaddy <sjoerd.weerstra@hotmail.com>
2017-02-11 15:04:03 +0100

    Use explicit const instead of trailing commas

--- app/scripts.babel/options.js
+++ app/scripts.babel/options.js
@@ -1,18 +1,18 @@
 (() => {
-    const typeSelect = document.getElementById('type'),
-        sortCheckbox = document.getElementById('cbSort'),
-        sortSelect = document.getElementById('sort'),
-        amountInput = document.getElementById('amount'),
-        yearsInput = document.getElementById('years');
+    const typeSelect = document.getElementById('type');
+    const sortCheck = document.getElementById('cbSort');
+    const sortSelect = document.getElementById('sort');
+    const amountInput = document.getElementById('amount');
+    const yearsInput = document.getElementById('years');
 
     document.addEventListener('DOMContentLoaded', () => {
         const options = JSON.parse(localStorage.getItem('options'));
 
         if (options) {
             typeSelect.value = options.type;
-            sortCheckbox.checked = Boolean(options.sort);
+            sortCheck.checked = Boolean(options.sort);
             sortSelect.value = options.sort;
-            if (options.years.length) {
+            if (options.years.length > 0) {
                 yearsInput.value = options.years.join(', ');
             }
             amountInput.value = options.amount;
@@ -22,7 +22,7 @@
     document.getElementById('save').addEventListener('click', () => {
         localStorage.setItem('options', JSON.stringify({
             type: typeSelect.value,
-            sort: sortCheckbox.checked ? sortSelect.value : null,
+            sort: sortCheck.checked ? sortSelect.value : null,
             years: yearsInput.value ? yearsInput.value.trim().split(/\s*,\s*/) : [],
             amount: +amountInput.value || null
         }));
